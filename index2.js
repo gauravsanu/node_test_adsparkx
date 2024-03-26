f5a88a778c78e08ba82eab396ed2b41a20293dfb
Gaurav Shrivastava <gauravsanu@gmail.com>
2024-03-26 21:24:28 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,24 +1,24 @@
 const redis = require("redis"); 
 const redisclient = redis.createClient(); // I Asume redis is running on the local machine on 6379 port
-  
-(async () => { 
+
+( async () => { 
     await redisclient.connect();
 })(); 
-  
+
 console.log("Connecting to the Redis"); 
-  
+
 redisclient.on("ready", () => { 
     redisclient.select(0);
     redisclient.set("CRICKET","GAME:1");
-	
+
 	redisclient.get("CRICKET").then((data) => { // Beacuse of async responce of Redis. Promise is retuen type
 		console.log(data); 
 	});
 
 console.log( "Connected" ); 
-    
+
 }); 
-  
+
 redisclient.on("error", (err) => { 
     console.log("Error in the Connection"); 
 });
